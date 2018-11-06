d00b609e9d14c33484555ad48c6b022f2579281f
Victor Marta <vicm159@gmail.com>
2018-11-06 13:11:58 -0800

    update

--- bash.js
+++ bash.js
@@ -2,16 +2,10 @@ process.stdout.write('prompt >');
 
 process.stdin.on('data', data => {
   const cmd = data.toString().trim();
-  if (cmd === 'pwd') {
-    let output = require('./pwd');
-    output();
-  }
 
-  if(cmd === 'ls'){
-    let output = require('./ls')
-    output()
-  }
+  let output = require(`./${cmd}`);
+  output();
 
-  //process.stdout.write(cmd);
+  process.stdout.write(cmd);
   process.stdout.write('\nprompt > ');
 });
